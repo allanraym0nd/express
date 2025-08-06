a401a962031cbddf70ec626216dfafc19bf9cfeb
allan raymond <raymonda916@gmail.com>
2025-08-06 07:08:06 +0300

    Update server.js: enhance server configuration

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
 import express from 'express'
 import path from 'path'
-import posts from './routes/posts.js';
+import { fileURLToPath } from 'url'
+import posts from './routes/postRoutes.js';
 import logger from './middleware/logger.js';
 import errorHandler from './middleware/error.js';
+import notFound from './middleware/notFound.js';
 const app = express()
 
 const port = process.env.PORT || 8000
 
+//get the directory name
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-
-
+// Body parser middleware
 app.use(express.json()) // raw data
 app.use(express.urlencoded({extended:false})) // form data
 
 //logger middleware
-
 app.use(logger)
+
+app.use(express.static(path.join(__dirname, 'public'))) // __dirname is in the scope of commonJs
 
 //Routes
 app.use('/api/posts', posts)
 
+// catch all
+app.use(notFound);
+
 //errorHandler middleware
 app.use(errorHandler)
 //set up static folder. we use, use() because its middleware
-
-// app.use(express.static(path.join(__dirname, 'public')))
 
 // app.get('/', (req,res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html')) // a way to serve http files instead of just json or etx 
